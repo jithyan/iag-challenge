4026cb1c72324a18dc02ff04fd4dc83ee88d7b87
moody-jnan <jithya.n@outlook.com>
2021-04-21 19:54:03 +1000

    express skeleton

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello world meo2w");
+import express from "express";
+import helmet from "helmet";
+
+const PORT = 5000;
+
+const server = express();
+server.use(helmet());
+server.use(express.json());
+
+server.listen(PORT, () => {
+  console.log("Listening on port: ", PORT);
+});
